35bdffed3bf48307c327875e236973871deddd19
adaml <adam.lemkhanter@e-campus.uab.cat>
2020-05-13 23:21:13 +0200

    Projecte buit

---
+++ scripts/Viatge.py
@@ -0,0 +1,6 @@
+class Viaje:
+    def __init__(self):
+        self.num_viajeros = 0  """ Numero de vaijeros"""
+        self.desinos  """ Lista/diccsionario de destinos """
+        self.datos_viajeros = []
+
