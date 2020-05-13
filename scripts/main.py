cb36280e40ceeaaf61981c44aa0ad4b67ef80d1b
adaml <adam.lemkhanter@e-campus.uab.cat>
2020-05-13 22:46:10 +0200

    Projecte buit

--- scripts/main.py
+++ scripts/main.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
         def __init__(self):
             self.num_viajeros = 0  """ Numero de vaijeros"""
             self.desinos = []  """ Lista/diccsionario de destinos """
-            self.datos_viajeros = [] """ """
+            self.datos_viajeros = []
