40209f7f14acc1a3050d3c79d92c1b836d6f73fe
adaml <adam.lemkhanter@e-campus.uab.cat>
2020-05-13 22:44:41 +0200

    Projecte buit

--- scripts/main.py
+++ scripts/main.py
@@ -1,2 +1,7 @@
 if __name__ == "__main__":
     print("Guillem subnormal Chupame el coxis")
+    class Viaje:
+        def __init__(self):
+            self.num_viajeros = 0  """ Numero de vaijeros"""
+            self.desinos = []  """ Lista/diccsionario de destinos """
+            self.datos_viajeros = [] """ """
