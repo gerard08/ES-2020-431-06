4326e87e0be1de1b3459c0b03cf1bfca6f13c8a5
1526000 <gerard.tejon@e-campus.uab.cat>
2020-05-18 04:43:28 +0200

    Funcionalitats afegides (metodes classe PaymentData.py i mes)

--- scripts/reserva.py
+++ scripts/reserva.py
@@ -1,13 +1,22 @@
 from . import User
 from . import Skyscanner
 from . import Flights
-from . import Rentalcars
+from . import Cars
 
 class Reserva:
 
     def __init__(self):
         self.preu = 0
+        self.usuari = None
+        v = Flights(10)
+        c = Cars(3)
+        self.vols = v.ConsultarLlista()
+        self.destinacions = []
         pass
+
+    def obtenir_usuari(self):
+        return self.usuari
+
 
 
     '''
