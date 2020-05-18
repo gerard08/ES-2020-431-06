2d867c6f01e19c560fbf1674f524be39c8148b75
Adam-byte-order <65297895+adam-byte-order@users.noreply.github.com>
2020-05-18 03:44:52 +0200

    Funció que l'Adam no ha pogut penjar

---
+++ scripts/reserva.py
@@ -0,0 +1,9 @@
+class Reserva:
+
+    def init(self):
+        pass
+
+    def Confirmar_reserva(Skyscaner,Rentalcars,Hotels):
+        Skyscaner.confirm_reserve(user: User, flights: Flights)
+        Rentalcars.confirm_reserve( user: User, cars: Cars)
+        Hotels.confirm_reserve()
