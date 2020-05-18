c87bda5d346448fed455091f58e9417b433f5df4
Guillem <guillem.santos@e-campus.uab.cat>
2020-05-18 03:07:45 +0200

    Class Hotels

--- scripts/Hotels.py
+++ scripts/Hotels.py
@@ -1,4 +1,9 @@
 class Hotels:
 
     def __init__(self):
+        self.codi = 0
+        self.nom = "none"
+        self.nhostes = 0
+        self.nhabitacions = 0
+        self.duradareserva = 0
         pass
--- scripts/Viatge.py
+++ scripts/Viatge.py
@@ -1,6 +1,6 @@
 class Viaje:
     def __init__(self):
-        self.num_viajeros = 0  """ Numero de vaijeros"""
+        self.num_viajeros = 0  """ Numero de viajeros"""
         self.desinos  """ Lista/diccsionario de destinos """
         self.datos_viajeros = []
 
