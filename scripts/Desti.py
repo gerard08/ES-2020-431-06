6134b1a92d0af88e2f6c82a832b2595cf6e71b4a
gerard_08 <gerardmaresp@gmail.com>
2020-05-18 04:47:43 +0200

    Classe destí afegida

---
+++ scripts/Desti.py
@@ -0,0 +1,12 @@
+class Desti:
+
+
+    def __init__(self, nom, COD):
+        self.__nom = nom
+        self.__COD = COD
+
+    def getNom(self):
+        return self.__nom
+
+    def getCOD(self):
+        return self.__COD
