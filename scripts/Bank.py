5c9b84247b086386348803eeaf0eaa3848f8a91c
1526000 <gerard.tejon@e-campus.uab.cat>
2020-05-18 02:50:09 +0200

    Algunes inicialitzacions

--- scripts/Bank.py
+++ scripts/Bank.py
@@ -5,7 +5,9 @@ from . import PaymentData
 class Bank:
 
     def __init__(self):
+
         pass
 
     def do_payment(self, user: User, payment_data: PaymentData):
+        #sumatori de tots els imports i cobrament
         return True
