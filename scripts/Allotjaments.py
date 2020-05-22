2abc749afcc6f7c8440e8c9bc28ce33998a42b7b
Guillem <guillem.santos@e-campus.uab.cat>
2020-05-22 17:05:35 +0200

    Getters a Allotjaments

--- scripts/Allotjaments.py
+++ scripts/Allotjaments.py
@@ -7,3 +7,17 @@ class Allotjaments:
         self.nomAllotjament = nomAllotjament
         self.__codi = codi
 
+        def getDireccio(self):
+            return self.__direccio
+
+        def getPreu(self):
+            return self.__preu
+
+        def getTipusAllotjament(self):
+            return self.__tipusAllotjament
+
+        def getNomAllotjament(self):
+            return self.nomAllotjament
+
+        def getCodi(self):
+            return self.codi
